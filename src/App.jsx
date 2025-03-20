a1946c0d190d6ab48e5a90baaf74c3f319f6ecb3
rajan <egoistrajan33@gmail.com>
2025-03-20 13:02:46 +0530

    Fixed git lock issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-red-500'>hello world</h1>
+      {/* <Login/> */}
+      <EmployeeDashboard/>
     </>
   )
 }
